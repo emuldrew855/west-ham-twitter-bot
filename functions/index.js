0aaf92da9a8dc62762f2cad3b73b57698fbb2bb7
Edward Muldrew <edward.muldrew@workday.com>
2022-09-16 16:32:18 +0100

    Add initial commit

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,34 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const TwitterApi = require("twitter-api-v2").default;
+const { Configuration, OpenAIApi } = require("openai");
 admin.initializeApp();
 
 // Database reference
-const dbRef = admin.firestore().doc('tokens/demo');
+const dbRef = admin.firestore().doc("tokens/demo");
 
 // Twitter API init
-const TwitterApi = require('twitter-api-v2').default;
 const twitterClient = new TwitterApi({
-  clientId: 'YOUR_CLIENT_ID',
-  clientSecret: 'YOUR_CLIENT_SECRET',
+  clientId: "S0lWd0JtYURpR0tHV1dib0sxRXA6MTpjaQ",
+  clientSecret: "62xoNTb1031TS7NB2hg76avakizYOlhtNZ04eDwxsoWAY6MdHy",
 });
 
-const callbackURL = 'http://127.0.0.1:5000/faxnow-app/us-central1/callback';
+const port = "5001";
+const callbackURL = `http://127.0.0.1:${port}/whubot-9aeb3/us-central1/callback`;
 
 // OpenAI API init
-const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
-  organization: 'YOUR_OPENAI_ORG',
-  apiKey: 'YOUR_OPENAI_SECRET',
+  organization: "org-FVKQYhm9HcjSioxaCqk8GfWe",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
 // STEP 1 - Auth URL
-exports.auth = functions.https.onRequest((request, response) => {
+exports.auth = functions.https.onRequest(async (request, response) => {
+  console.log(`Authentication started`);
   const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
     callbackURL,
-    { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] }
+    { scope: ["tweet.read", "tweet.write", "users.read", "offline.access"] }
   );
 
   // store verifier
@@ -35,15 +37,15 @@ exports.auth = functions.https.onRequest((request, response) => {
   response.redirect(url);
 });
 
-// STEP 2 - Verify callback code, store access_token 
-exports.callback = functions.https.onRequest((request, response) => {
+// STEP 2 - Verify callback code, store access_token
+exports.callback = functions.https.onRequest(async (request, response) => {
   const { state, code } = request.query;
 
   const dbSnapshot = await dbRef.get();
   const { codeVerifier, state: storedState } = dbSnapshot.data();
 
   if (state !== storedState) {
-    return response.status(400).send('Stored tokens do not match!');
+    return response.status(400).send("Stored tokens do not match!");
   }
 
   const {
@@ -55,7 +57,7 @@ exports.callback = functions.https.onRequest((request, response) => {
     codeVerifier,
     redirectUri: callbackURL,
   });
-
+  console.log(`Access Token: ${accessToken}, refreshToken: ${refreshToken}`);
   await dbRef.set({ accessToken, refreshToken });
 
   const { data } = await loggedClient.v2.me(); // start using the client if you want
@@ -64,9 +66,10 @@ exports.callback = functions.https.onRequest((request, response) => {
 });
 
 // STEP 3 - Refresh tokens and post tweets
-exports.tweet = functions.https.onRequest((request, response) => {
+exports.tweet = functions.https.onRequest(async (request, response) => {
+  const prompt = `tweet something about ${matchDayPrompts[0]}`;
+  console.log(`Prompt: ${prompt}`);
   const { refreshToken } = (await dbRef.get()).data();
-
   const {
     client: refreshedClient,
     accessToken,
@@ -75,14 +78,39 @@ exports.tweet = functions.https.onRequest((request, response) => {
 
   await dbRef.set({ accessToken, refreshToken: newRefreshToken });
 
-  const nextTweet = await openai.createCompletion('text-davinci-001', {
-    prompt: 'tweet something cool for #techtwitter',
+  const nextTweet = await openai.createCompletion("text-davinci-002", {
+    prompt,
     max_tokens: 64,
   });
 
-  const { data } = await refreshedClient.v2.tweet(
-    nextTweet.data.choices[0].text
-  );
+  const tweet = `${nextTweet.data.choices[0].text} #COYI #IRONS`;
+  console.log(`Tweeting: ${tweet}`);
+  const { data } = await refreshedClient.v2.tweet(tweet);
 
   response.send(data);
 });
+
+// Setting local timeout
+exports.cronTweet = functions.https.onRequest(async (request, response) => {
+  // tweets every 10 seconds
+  await setInterval(this.tweet, 28800000, request, response);
+  response.send("Sending out tweets every 10 seconds");
+});
+
+// Step 4 - Run on cron job
+exports.tweetHourly = functions.pubsub
+  .schedule("*/5 * * * *")
+  .onRun(async (context) => {
+    console.log("Tweeting every 5 minutes");
+    this.tweet();
+  });
+
+const matchDayPrompts = ["West Ham vs Everton", "West Ham vs Wolves"];
+const playerPrompts = [
+  "Alphonse Aréola",
+  "Łukasz Fabiański",
+  "Darren Randolph",
+  "Vladimir Coufal",
+  "Jarod Bowen",
+  "Gianluca Scamacca",
+];
